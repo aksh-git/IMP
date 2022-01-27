281b638cf75c4e70de12ca19cba691419a19db1f
aksh-git <ann0m@protonmail.com>
2022-01-27 15:59:18 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-const basicLMPLayout = `
-    <div id="LMPLayoutContainer">
-      <div id="LMPLayoutFront">
-        <div id="LMPMusicLogo">
-          <img width="50px" id="LMPMusicLogoIcon" src="./assets/images/icons/defaultMusicIcon.jpg">
+const basicIMPLayout = `
+    <div id="IMPLayoutContainer">
+      <div id="IMPLayoutFront">
+        <div id="IMPMusicLogo">
+          <img width="50px" id="IMPMusicLogoIcon" src="./assets/images/icons/defaultMusicIcon.jpg">
         </div>
       </div>
-      <div id="LMPLayoutBottom">
-        <div id="LMPbtns">  
+      <div id="IMPLayoutBottom">
+        <div id="IMPbtns">  
             <i id="playPauseBtn" onclick="play(this)" class="fa fa-play-circle-o" aria-hidden="true"></i>
         </div>
       </div>
     </div>
 `;
-let LMPLayout = document.getElementById("LMPLayout");
+let IMPLayout = document.getElementById("IMPLayout");
 
-function initLMPLayout(){
-    LMPLayout.innerHTML=basicLMPLayout;
+function initIMPLayout(){
+    IMPLayout.innerHTML=basicIMPLayout;
 }
 
 window.onload = () =>{
-    initLMPLayout();
+    initIMPLayout();
 }
