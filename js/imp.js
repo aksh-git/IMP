8e4e8a2bc9f8aa42c6d9148c5bd64505088da3c3
aksh-git <ann0m@protonmail.com>
2022-01-29 18:59:16 +0530

    update

--- js/imp.js
+++ js/imp.js
@@ -8,20 +8,19 @@ const basicIMPLayout = `
         </div>
       </div>
       <div id="IMPLayoutBottom">
+      <span></span>
         <div id="IMPbtns">  
-            <i id="playPauseBtn" onclick="play(this)" class="fa fa-play-circle" aria-hidden="true"></i>
+            <i title="Play/Pause" id="playPauseBtn" onclick="play(this)" class="fa fa-play-circle" aria-hidden="true"></i>
         </div>
         <div id="IMPbtns_2">
           <i title="Load New Play-List" onclick="refreshSongs(this)" class="white fa fa-refresh" aria-hidden="true"></i>
           <i title="More Info" onclick="moreInfo()" class="white fa fa-info-circle" aria-hidden="true"></i>
         </div>
       </div>
-      <span></span>
     </div>
 `;
 let IMPLayout = document.getElementById("IMPLayout");
 let playerbg;
-
 
 function initIMPLayout(){
     IMPLayout.innerHTML=basicIMPLayout;
@@ -37,7 +36,7 @@ function random_bg_color() {
 }
 
 function moreInfo(){
-  window.open("https://github.com/aksh-git/IMP#imp---js");
+  window.open("https://github.com/aksh-git/IMP#readme");
 }
 
 window.onload = () =>{
